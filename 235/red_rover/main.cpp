a8bc2fd2d01dbe76e4fb42e74259ec3d3ebe68d9
Spencer Wood <spencerwood@MacBook-Pro.local>
2014-11-04 00:57:41 -0700

    starting red rover

---
+++ 235/red_rover/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "Red Rover" << endl << endl
+      << "1. Create Roster" << endl
+      << "2. View Roster" << endl
+      << "3. Shuffle Roster" << endl
+      << "4. Create Teams" << endl
+      << "5. View Teams" << endl
+      << "6. User Play" << endl
+      << "7. Auto Play" << endl
+      << "8. Quit" << endl << endl
+      << "Please make a selection: " << endl;
+   return 0;
+}
