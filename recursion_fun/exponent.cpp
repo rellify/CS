5564d0c4a40607717bc117507c7176658fe22ac5
scwood <spencercwood@gmail.com>
2014-11-11 19:20:56 -0700

    yeah

---
+++ recursion_fun/exponent.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Calculates x^n
+ * @param   x  operand
+ * @param   n  power
+ * @return     the answer
+ */
+int exponent(int x, int n) {
+   if (n == 0) {
+      return 1;
+   }
+   else return x * exponent(x, n-1);
+}
+int main() {
+   int x;
+   int n;
+   cout << "------------------------------" << endl;
+   cout << "x term = ";
+   cin >> x;
+   cout << "n term = ";
+   cin >> n;
+   cout << x << "^" << n << " = " << exponent(x,n) << endl;
+   cout << "------------------------------" << endl;
+   return 0;
+}
+
+
+
