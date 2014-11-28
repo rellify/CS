416b8765339364d4a44c040dc8b8eda51af59034
scwood <spencercwood@gmail.com>
2014-11-28 01:42:15 -0700

    asdf

--- 235/lab08/QS.cpp
+++ 235/lab08/QS.cpp
@@ -121,9 +121,9 @@ bool QS::createArray(int size) {
 	}
 	array = new int[size];
 	array_size = size;
-	// for (int i = 0; i < array_size; i++) {
-	// 	array[i] = 0;
-	// }
+	for (int i = 0; i < array_size; i++) {
+		array[i] = 0;
+	}
 	return true;
 }
 
